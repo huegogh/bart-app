24b696c1d4dbcb39ff65b0ebc95394d8b8c8c56f
JohnCodePass <jmoralesc0259@gmail.com>
2021-03-19 10:28:20 -0700

    Got depart api to call

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from 'rxjs';
 import { IAdvisory } from '../interfaces/advisory'
+import { IDeparture } from '../interfaces/depature';
 
 @Injectable({
   providedIn: 'root'
@@ -12,14 +13,17 @@ export class ApiService {
   calcUrl = '';
   scheduleUrl = '';
   platformUrl = '';
-  departuresUrl = '';
+  departuresUrl = 'https://api.bart.gov/api/etd.aspx?cmd=etd&orig=RICH&key=MW9S-E7SL-26DU-VV8V&json=y';
 
 
   constructor(private http: HttpClient) { }
 
-  
   GetHomeInfo(): Observable<IAdvisory>{
     return this.http.get<IAdvisory>(this.homeUrl);
+  }
+
+  GetDepartInfo(): Observable<IDeparture>{
+    return this.http.get<IDeparture>(this.departuresUrl);
   }
 
 
